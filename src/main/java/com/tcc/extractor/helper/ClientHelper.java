3b230ff9c69b8c0998d3f045ef6351a769ba61da
Willian Alves <wba.alves@gmail.com>
2020-09-07 12:14:32 -0300

    create a client helper

---
+++ src/main/java/com/tcc/extractor/helper/ClientHelper.java
@@ -0,0 +1,37 @@
+package com.tcc.extractor.helper;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientHelper {
+
+  public List<String> getDirectoryApiUrl(List<String> urls) {
+    return urls.stream()
+      .map(this::getUserAndRepo)
+      .map(this::getDirectoryFormattedApiUrl)
+      .collect(Collectors.toList());
+  }
+
+  private String[] getUserAndRepo(String gitUrl) {
+    Pattern pattern = Pattern.compile("\\.com/(.*)");
+    Matcher matcher = pattern.matcher(gitUrl);
+    return matcher.group().split("/");
+  }
+
+  private String getDirectoryFormattedApiUrl(String[] userAndRepo) {
+    StringBuilder sb = new StringBuilder();
+    String user = userAndRepo[0];
+    String repo = userAndRepo[1];
+    return sb.append("https://api.github.com/repos/")
+      .append(user)
+      .append("/")
+      .append(repo)
+      .append("/contents")
+      .toString();
+  }
+}
